8d095a1a3bb84ccc38b4400e9b7082682eaa22ac
dennis.adinata <dennistok96@gmail.com>
2023-01-08 23:39:36 +0700

    update pom

--- src/test/java/parallel/FailedRunnerTest.java
+++ src/test/java/parallel/FailedRunnerTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.DataProvider;
 @CucumberOptions(features = "@target/failedrerun.txt",glue = "parallel",plugin = {"pretty",
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
         "rerun:target/failedrerun.txt"})
-public class FailedRunner extends AbstractTestNGCucumberTests {
+public class FailedRunnerTest extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
--- src/test/java/parallel/ParallelRunnerTest.java
+++ src/test/java/parallel/ParallelRunnerTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.DataProvider;
         "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
         "rerun:target/failedrerun.txt"},
         tags="not @Skip")
-public class ParallelRunner extends AbstractTestNGCucumberTests {
+public class ParallelRunnerTest extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
